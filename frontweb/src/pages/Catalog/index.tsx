a0d68c799203d903db45c4e4b4e86317337aeb09
helderargomes <helderargomes@gmail.com>
2025-01-12 17:00:21 -0300

    Fix AxiosRequestConfig

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 import { Product } from "types/product";
 import { useEffect, useState } from "react";
 import { SpringPage } from "types/vendor/spring";
-import { AxiosParams } from "types/vendor/axios";
 import { BASE_URL } from "util/requests";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 import "./styles.css";
 import CardLoader from "./CardLoader";
@@ -16,9 +15,10 @@ const Catalog = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const params: AxiosParams = {
+    const params: AxiosRequestConfig = {
       method: "GET",
-      url: `${BASE_URL}/products`,
+      url: "/products",
+      baseURL: BASE_URL,
       params: {
         page: 0,
         size: 12,
@@ -41,13 +41,17 @@ const Catalog = () => {
         <h1>Catálogo de produtos</h1>
       </div>
       <div className="row">
-        {isLoading ? <CardLoader /> : (page?.content.map((product) => (
-          <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
-            <Link to="/products/1">
-              <ProductCard product={product} />
-            </Link>
-          </div>
-        )))}
+        {isLoading ? (
+          <CardLoader />
+        ) : (
+          page?.content.map((product) => (
+            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+              <Link to="/products/1">
+                <ProductCard product={product} />
+              </Link>
+            </div>
+          ))
+        )}
       </div>
 
       <div className="row">
