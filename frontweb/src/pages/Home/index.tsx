5de28792e474938989d06f794e6fb39b643c72c5
helderargomes <helderargomes@gmail.com>
2025-01-17 22:50:50 -0300

    PrivateRoute

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -3,12 +3,10 @@ import { ReactComponent as MainImage } from "assets/images/Desenho.svg";
 import "./styles.css";
 import ButtonIcon from "components/ButtonIcon";
 import { Link } from "react-router-dom";
-import {isAuthenticated} from "util/requests";
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
