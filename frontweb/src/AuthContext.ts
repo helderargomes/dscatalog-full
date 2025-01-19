d514cd009e7cbe27283b0e53d77e307e3f343fc5
helderargomes <helderargomes@gmail.com>
2025-01-18 23:34:50 -0300

    Global state with context API

---
+++ frontweb/src/AuthContext.ts
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { TokenData } from "util/requests";
+
+export type AuthContextData = {
+  authenticated: boolean;
+  tokenData?: TokenData;
+};
+
+export type AuthContextType = {
+  authContextData: AuthContextData;
+  setAuthContextData: (authcontextData: AuthContextData) => void;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  authContextData: {
+    authenticated: false,
+  },
+  setAuthContextData: () => null,
+});
--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -3,8 +3,9 @@ import ButtonIcon from "components/ButtonIcon";
 import { useForm } from "react-hook-form";
 
 import "./styles.css";
-import { getAuthData, requestBackendLogin, saveAuthData } from "util/requests";
-import { useState } from "react";
+import { getTokenData, requestBackendLogin, saveAuthData } from "util/requests";
+import { useContext, useState } from "react";
+import { AuthContext } from "AuthContext";
 
 type FormData = {
   username: string;
@@ -12,6 +13,8 @@ type FormData = {
 };
 
 const Login = () => {
+
+  const { setAuthContextData } = useContext(AuthContext);
   
   const [hasError, setHasError] = useState(false);
 
@@ -23,9 +26,11 @@ const Login = () => {
     requestBackendLogin(formData)
       .then((response) => {
         saveAuthData(response.data);
-        const token = getAuthData().access_token;
-        console.log('TOKEN GERADO: ' + token);
-        console.log("SUCESSO", response);
+        setHasError(false);
+        setAuthContextData({
+                authenticated: true,
+                tokenData: getTokenData(),
+              });
         history.push('/admin');
       })
       .catch((error) => {
